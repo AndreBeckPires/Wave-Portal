99d0b6f7bc459a23cdfb74a9f42525e115597868
Pires <a.pires@sap.com>
2022-07-21 19:19:03 -0300

    deploying

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,11 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-waffle");
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.0",
+  networks: {
+    rinkeby: {
+      url: "https://eth-rinkeby.alchemyapi.io/v2/NxqTpvtF2Oc8wO4Yl8A__1MD-fkG2bgm",
+      accounts: ["e3e1b92aba1c2e4b657c4a6f25cee54108cee8f54633b1533a8b29136f402de1"],
+    },
+  },
 };
